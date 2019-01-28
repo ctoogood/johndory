803fe4f351aa08a779a319baa214af8fcb553b45
ctoogood <calumtoogood@gmail.com>
2019-01-28 14:38:40 +0000

    changed blog post title

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -4,14 +4,9 @@ const typography = new Typography({
   baseFontSize: "18px",
   baseLineHeight: 1.666,
   headerFontFamily: [
-    "Playfair Display",
-    "Helvetica Neue",
-    "Segoe UI",
-    "Helvetica",
-    "Arial",
-    "sans-serif",
+    "Playfair Display"
   ],
-  bodyFontFamily: ["Montserrat", "serif"],
+  bodyFontFamily: ["Montserrat"],
 })
 
 export default typography
