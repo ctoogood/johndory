77d95136de3c2d0b074feca114c7380fd82f1de8
ctoogood <calumtoogood@gmail.com>
2019-07-19 16:55:56 +0100

    edited analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,11 +13,6 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-143662307-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "johndory.uk",
       },
     },
 
